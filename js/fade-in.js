3cb0515771268a2dc2c4703cd5b9ee550c9cadff
Bradley Murchison <39600496+Brad2424@users.noreply.github.com>
2018-08-28 21:00:23 -0500

    Fade in js (#2)
    * Initial commit
    
    * initial JS added
    
    * complete functionality added
    
    Still need to look for perfomance improvements (perhaps replicate the code used for the moving images in header).
    
    * default active given for longer displays
    
    * fade in point adjusted higher
    
    * Merge branch 'master' into fade-in-JS
    
    * debounce function wait time decreased
    
    * removed "executed" message from console

---
+++ js/fade-in.js
@@ -0,0 +1,34 @@
+function debounce(func, wait = 30, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+const fadeInElements = document.querySelectorAll('.fade-in');
+
+function checkFadeIn() {
+  fadeInElements.forEach(fadeInElement => {
+    // upper part of the element
+    const slideInAt = (window.scrollY + window.innerHeight) - (fadeInElement.clientHeight / 200);
+    // bottom of the element
+    const elementBottom = fadeInElement.offsetTop + fadeInElement.clientHeight;
+    const isHalfShown = slideInAt > fadeInElement.offsetTop;
+    const isNotScrolledPast = window.scrollY < elementBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      fadeInElement.classList.add('active-fade-in');
+    } else {
+      fadeInElement.classList.remove('active-fade-in');
+    }
+  });
+}
+
+window.addEventListener('scroll', debounce(checkFadeIn));
